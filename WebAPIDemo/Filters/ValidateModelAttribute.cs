d209146d276553aba421230844eba25ee40d71d1
eddie <bda605@gmail.com>
2017-05-28 22:01:17 +0800

    加入Web Api Model Validation

--- WebAPIDemo/Filters/ValidateModelAttribute.cs
+++ WebAPIDemo/Filters/ValidateModelAttribute.cs
@@ -9,27 +9,17 @@ using System.Web.Http.ModelBinding;
 using System.Net;
 namespace WebAPIDemo.Filters
 {
-    //public class ValidateModelAttribute:ActionFilterAttribute
-    //{
-    //    public override void OnActionExecuting(HttpActionContext actionContext)
-    //    {
-    //        if (actionContext.ModelState.IsValid == false) 
-    //        {
-    //            actionContext.Response = 
-    //            actionContext.Request.CreateErrorResponse
-    //            (HttpStatusCode.BadRequest, actionContext.ModelState);
-    //        }
-    //    }
-    //}
     public class ValidateModelAttribute : ActionFilterAttribute
     {
         public override void OnActionExecuting(HttpActionContext actionContext)
         {
             if (actionContext.ModelState.IsValid == false)
             {
-                actionContext.Response = actionContext.Request.CreateErrorResponse(
-                    HttpStatusCode.BadRequest, actionContext.ModelState);
+                actionContext.Response =
+                actionContext.Request.CreateErrorResponse
+                (HttpStatusCode.BadRequest, actionContext.ModelState);
             }
         }
     }
+    
 }
