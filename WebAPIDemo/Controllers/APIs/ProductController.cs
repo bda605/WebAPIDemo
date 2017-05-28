4618f020d1b2dae2a06f66bb042368de5cde1852
eddie <bda605@gmail.com>
2017-05-28 20:29:46 +0800

    API第一次簽入

---
+++ WebAPIDemo/Controllers/APIs/ProductController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPIDemo.Models;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+
+namespace WebAPIDemo.Controllers.APIs
+{
+    public class ProductController : ApiController
+    {
+        private NorthwindEntities db = new NorthwindEntities();
+        public IEnumerable<Products> GetProducts()
+        {
+            return db.Products.AsEnumerable();
+        }
+
+        public Products GetProduct(int id)
+        {
+            Products product = db.Products.Find(id);
+            if (product == null) throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound));     
+            return product;
+        }
+
+        public HttpResponseMessage PutProduct(int id, Products product)
+        {
+            if (ModelState.IsValid && id == product.ProductID)
+            {
+                db.Entry(product).State = EntityState.Modified;
+                try
+                {
+                    db.SaveChanges();
+                }
+                catch (DbUpdateException)
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound);
+                }
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+        }
+        public HttpResponseMessage PostProduct(Products product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Products.Add(product);
+                db.SaveChanges();
+                HttpResponseMessage reponse = Request.CreateResponse(HttpStatusCode.Created, product);
+                reponse.Headers.Location = new Uri(Url.Link("DefaultApi", new { id = product.ProductID }));
+                return reponse;
+            }
+            else {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+        }
+        public HttpResponseMessage DeleteProduct(int Id) 
+        {
+            Products product = db.Products.Find(Id);
+            if (product == null) return Request.CreateResponse(HttpStatusCode.NotFound);
+            db.Products.Remove(product);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException) 
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, product);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebAPIDemo/Models/DataAnnotations/ProductMetadata.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace WebAPIDemo.Models
+{
+    [MetadataType(typeof(ProductMetadata))]
+    public partial class Products
+    {
+        public class ProductMetadata
+        {
+            [ScaffoldColumn(false)]
+            public int ProductID { get; set; }
+            [Required(ErrorMessage = "產品名稱不得空白")]
+            [StringLength(40, MinimumLength = 3, ErrorMessage = "產品名稱長度介於3-40")]
+            public string ProductName { get; set; }
+
+            public Nullable<int> SupplierID { get; set; }
+            public Nullable<int> CategoryID { get; set; }
+            [StringLength(20)]
+            public string QuantityPerUnit { get; set; }
+            public Nullable<decimal> UnitPrice { get; set; }
+            public Nullable<short> UnitsInStock { get; set; }
+            public Nullable<short> UnitsOnOrder { get; set; }
+            public Nullable<short> ReorderLevel { get; set; }
+            public bool Discontinued { get; set; }
+        }
+    }
+}
