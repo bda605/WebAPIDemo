b8ce1a4e0a381b07943468e7e75bab7c522ccbeb
eddie <bda605@gmail.com>
2017-05-28 23:36:51 +0800

    加入Repository

---
+++ WebAPIDemo/Models/ProductRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using WebAPIDemo.Models.Repository;
+namespace WebAPIDemo.Models
+{
+    public class ProductRepository:IProductRepository
+    {
+        private NorthwindEntities db;
+        public ProductRepository() 
+        {
+            db = new NorthwindEntities();
+        }
+        public IEnumerable<Products> GetAll() 
+        {
+            return db.Products.AsEnumerable();
+        }
+        public Products GetById(int Id) 
+        {
+            return db.Products.Find(Id);
+        }
+
+        public void Create(Products p) 
+        {
+            db.Products.Add(p);
+            db.SaveChanges();
+        }
+        public void Update(Products p) 
+        {
+            db.Entry(p).State = EntityState.Modified;
+            db.SaveChanges();
+        }
+        public void Delete(Products p) 
+        {
+            db.Entry(p).State = EntityState.Deleted;
+            db.SaveChanges();
+        }
+        public void Dispose() 
+        {
+            db.Dispose();
+        }
+    }
+}
---
+++ WebAPIDemo/Models/Repository/IProductRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPIDemo.Models.Repository
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Products> GetAll();
+        Products GetById(int Id);
+
+        void Create(Products p);
+        void Update(Products p);
+
+        void Delete(Products p);
+
+        void Dispose();
+    }
+}
